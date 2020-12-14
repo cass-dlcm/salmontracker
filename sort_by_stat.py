591ee619b9e727898c4e6f893c0d1689a6c8c09b
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2020-12-13 22:46:45 -0500

    added support for sorting stages

--- sort_by_stat.py
+++ sort_by_stat.py
@@ -1,24 +1,44 @@
 from salmontracker import initUser, locale, hasWeapon, doesntHaveWeapon, avgStat
 from utility import grizzcoWeapons
 import requests
+import pprint
+
+def sortWeapons(data: list) -> None:
+    weaponsList = requests.get("https://stat.ink/api/v2/weapon").json()
+    for weapon in grizzcoWeapons:
+        new = {
+            "name": {
+                locale: weapon[0]
+            },
+            "key": weapon[1]
+        }
+        weaponsList.append(new)
+    results = []
+    for weapon in weaponsList:
+        result = {}
+        withVal = list(filter(hasWeapon(weapon["name"][locale]), data))
+        withoutVal = list(filter(doesntHaveWeapon(weapon["name"][locale]), data))
+        if (len(withVal) > 0):
+            result["name"] = weapon["name"][locale]
+            result["value"] = avgStat(withVal, "clear_waves") - avgStat(withoutVal, "clear_waves")
+            results.append(result)
+    pprint.pprint(sorted(results, key = lambda val: val["value"]))
+
+def sortStages(data: list) -> None:
+    stageDict = {}
+    stageList = []
+    for job in data:
+        if not (job["stage"]["name"][locale] in stageDict):
+            stageDict[job["stage"]["name"][locale]] = {
+                "name": job["stage"]["name"][locale],
+                "clear_waves": 0.0,
+                "count": 0.0
+            }
+        stageDict[job["stage"]["name"][locale]]["clear_waves"] += job["clear_waves"]
+        stageDict[job["stage"]["name"][locale]]["count"] += 1.0
+    for stage in stageDict.values():
+        stageList.append({"name": stage["name"], "value": stage["clear_waves"] / stage["count"]})
+    pprint.pprint(sorted(stageList, key = lambda val: val["value"]))
 
 data = initUser()
-weaponsList = requests.get("https://stat.ink/api/v2/weapon").json()
-for weapon in grizzcoWeapons:
-    new = {
-        "name": {
-            locale: weapon[0]
-        },
-        "key": weapon[1]
-    }
-    weaponsList.append(new)
-results = []
-for weapon in weaponsList:
-    result = {}
-    withVal = list(filter(hasWeapon(weapon["name"][locale]), data))
-    withoutVal = list(filter(doesntHaveWeapon(weapon["name"][locale]), data))
-    if (len(withVal) > 0):
-        result["name"] = weapon["name"][locale]
-        result["value"] = avgStat(withVal, "clear_waves") - avgStat(withoutVal, "clear_waves")
-        results.append(result)
-print(sorted(results, key = lambda val: val["value"]))
+sortStages(data)
