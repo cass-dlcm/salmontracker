970cc3dd051d6482e19f78ef93f16ab9b5e05346
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2020-12-18 12:04:19 -0500

    make sure dangerrate_and_quota.py doesn't trigger accidentally

--- dangerrate_and_quota.py
+++ dangerrate_and_quota.py
@@ -1,22 +1,24 @@
-from core import initUser, getArrayOfStat, getArrayOfStat2D
+from core import initAll, getArrayOfStat, getArrayOfStat2D
+import core
 import matplotlib.pyplot as plt
 import numpy
-import json
+import ujson
 from typing import List
 
 
-initUser(json.load(open("keys.json", "r"))["statink_key"])
-path: str = "data/"
-data: str = "salmon.jsonl"
-dangerRates: List[float] = getArrayOfStat(path + data, "danger_rate")
-quotas: List[float] = getArrayOfStat2D(path + data, "quota", 2)
-plt.figure(1)
-plt.scatter(quotas, dangerRates)
-m, b = numpy.polyfit(quotas, dangerRates, 1)
-y: List[float] = []
-for i in quotas:
-    y.append(i * m + b)
-plt.plot(quotas, y)
-plt.xlabel("Wave 3 Quota")
-plt.ylabel("Hazard Level")
-plt.show()
+if __name__ == "__main__":
+    path: str = "data/"
+    data: str = "salmonAll.jl.gz"
+    dangerRates: List[float] = getArrayOfStat(path + data, "danger_rate")
+    quotas: List[float] = getArrayOfStat2D(path + data, "quota", 2)
+    plt.figure(1)
+    plt.scatter(quotas, dangerRates)
+    m, b = numpy.polyfit(quotas, dangerRates, 1)
+    y: List[float] = []
+    for i in quotas:
+        y.append(i * m + b)
+    plt.plot(quotas, y)
+    plt.xlabel("Wave 3 Quota")
+    plt.ylabel("Hazard Level")
+    print(core.jobsCount(path + data))
+    plt.show()
