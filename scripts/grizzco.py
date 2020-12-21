064a81e6e2337b6873e70b1aab9d956c4c9250bb
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2020-12-21 13:45:45 -0500

    add more statistics

--- scripts/grizzco.py
+++ scripts/grizzco.py
@@ -4,7 +4,7 @@ sys.path.insert(0, ".")
 import core
 import numpy as np
 from scipy.stats import ttest_ind
-from typing import List, cast, Union, Tuple
+from typing import List, cast, Tuple
 import matplotlib.pyplot as plt
 import jsonlines
 import ujson
@@ -22,7 +22,9 @@ if __name__ == "__main__":
             "Grizzco Slosher",
         ),
     )
-    result = filters.duringRotationInts(data, grizzcoRotationsInts, "or")
+    result: Tuple[str, str] = filters.duringRotationInts(
+        data, grizzcoRotationsInts, "or"
+    )
     withVal: str = result[0]
     withoutVal: str = result[1]
     withValClearWaves: List[float] = []
@@ -33,6 +35,10 @@ if __name__ == "__main__":
     withoutValDangerRate: List[float] = []
     withoutValGoldenTotal: List[float] = []
     withoutValPowerTotal: List[float] = []
+    clearWaves: List[float] = []
+    dangerRate: List[float] = []
+    goldenTotal: List[float] = []
+    powerTotal: List[float] = []
     with gzip.open(withVal) as reader:
         for job in jsonlines.Reader(reader, ujson.loads):
             withValClearWaves.append(float(job["clear_waves"]))
@@ -45,8 +51,21 @@ if __name__ == "__main__":
             withoutValDangerRate.append(float(job["danger_rate"]))
             withoutValGoldenTotal.append(float(job["my_data"]["golden_egg_delivered"]))
             withoutValPowerTotal.append(float(job["my_data"]["power_egg_collected"]))
+    with gzip.open(data) as reader:
+        for job in jsonlines.Reader(reader, ujson.loads):
+            clearWaves.append(float(job["clear_waves"]))
+            dangerRate.append(float(job["danger_rate"]))
+            goldenTotal.append(float(job["my_data"]["golden_egg_delivered"]))
+            powerTotal.append(float(job["my_data"]["power_egg_collected"]))
     t, p = ttest_ind(withValClearWaves, withoutValClearWaves, equal_var=False)
-    print("a - b = " + str(np.mean(withValClearWaves) - np.mean(withoutValClearWaves)))
+    diffMeansClearWaves: float = np.mean(withValClearWaves) - np.mean(
+        withoutValClearWaves
+    )
+    print("n_1 = " + str(len(withValClearWaves)))
+    print("n_2 = " + str(len(withoutValClearWaves)))
+    print()
+    print("a - b = " + str(diffMeansClearWaves))
+    print("d = " + str(diffMeansClearWaves / np.std(clearWaves)))
     print("t = " + str(t))
     print("p = " + str(p))
     print()
@@ -60,7 +79,11 @@ if __name__ == "__main__":
     plt.hist(withoutValDangerRate, density=True)
     plt.xlabel("Danger Rate")
     plt.ylabel("Probability")
-    print("a - b = " + str(np.mean(withValDangerRate) - np.mean(withoutValDangerRate)))
+    diffMeansDangerRate: float = np.mean(withValDangerRate) - np.mean(
+        withoutValDangerRate
+    )
+    print("a - b = " + str(diffMeansDangerRate))
+    print("d = " + str(diffMeansDangerRate / np.std(dangerRate)))
     print("t = " + str(t))
     print("p = " + str(p))
     print()
@@ -74,9 +97,11 @@ if __name__ == "__main__":
     plt.hist(withoutValGoldenTotal, density=True)
     plt.xlabel("Golden Eggs")
     plt.ylabel("Probability")
-    print(
-        "a - b = " + str(np.mean(withValGoldenTotal) - np.mean(withoutValGoldenTotal))
+    diffMeansGoldenTotal: float = np.mean(withValGoldenTotal) - np.mean(
+        withoutValGoldenTotal
     )
+    print("a - b = " + str(diffMeansGoldenTotal))
+    print("d = " + str(diffMeansGoldenTotal / np.std(goldenTotal)))
     print("t = " + str(t))
     print("p = " + str(p))
     print()
@@ -90,7 +115,9 @@ if __name__ == "__main__":
     plt.hist(withoutValPowerTotal, density=True)
     plt.xlabel("Power Eggs")
     plt.ylabel("Probability")
-    print("a - b = " + str(np.mean(withValPowerTotal) - np.mean(withoutValPowerTotal)))
+    diffMeansPowerEggs = np.mean(withValPowerTotal) - np.mean(withoutValPowerTotal)
+    print("a - b = " + str(diffMeansPowerEggs))
+    print("d = " + str(diffMeansPowerEggs / np.std(powerTotal)))
     print("t = " + str(t))
     print("p = " + str(p))
     print()
