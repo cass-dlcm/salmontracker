379a84904bac730738a07dd9cb70385e84d3e01f
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2020-12-22 17:57:45 -0500

    create script for getting percentage of tides

---
+++ scripts/tides.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.path.insert(0, ".")
+import core
+import jsonlines
+import ujson
+import gzip
+from typing import Dict
+
+if __name__ == "__main__":
+    data = "data/salmonAll.jl.gz"   # core.init("All")
+    tideDict: Dict[str, dict] = {
+        "high": {"key": "high", "count": 0.0},
+        "normal": {"key": "normal", "count": 0.0},
+        "low": {"key": "low", "count": 0.0},
+    }
+    total = 0.0
+    with gzip.open(data) as reader:
+        for job in jsonlines.Reader(reader, ujson.loads):
+            for wave in job["waves"]:
+                total += 1.0
+                tideDict[wave["water_level"]["key"]]["count"] += 1.0
+    for tide in tideDict.values():
+        print(tide["key"] + ": " + str(tide["count"] / total))
