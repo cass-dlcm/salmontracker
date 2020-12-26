6e7f9057f95d0b8998ece1845354c993554f2d24
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2020-12-26 14:53:56 -0500

    make scripts/rank_and_danger_rate.py use the new object format

--- scripts/rank_and_danger_rate.py
+++ scripts/rank_and_danger_rate.py
@@ -2,6 +2,7 @@ import sys
 
 sys.path.insert(0, ".")
 import core
+from objects import Job
 import matplotlib.pyplot as plt
 import numpy
 from typing import List, cast, Union, Tuple
@@ -10,20 +11,20 @@ import ujson
 import gzip
 
 if __name__ == "__main__":
-    data = core.init("All")
+    data = "data/salmonAll.jl.gz"  # core.init("All")
     x = []
     y = []
     with gzip.open(data) as reader:
-        for job in jsonlines.Reader(reader, ujson.loads):
-            if "title" in job and "title_exp" in job and job["title"] is not None:
-                x.append(job["title"]["splatnet"] * 100 + job["title_exp"])
-                y.append(float(job["danger_rate"]))
+        for line in reader:
+            job = Job(**ujson.loads(line))
+            if job.title is not None:
+                x.append(job.title.splatnet * 100 + job.title_exp)
+                y.append(float(job.danger_rate))
     plt.scatter(x, y)
     plt.xlabel("Rank")
     plt.ylabel("Hazard Level")
-    log_x = numpy.log(x)
-    m, b = numpy.polyfit(log_x, y, 1)
-    y_equation = list(sorted([x_val * m + b for x_val in log_x]))
-    print("y = {}log(x) + {}".format(m, b))
+    m, b = numpy.polyfit(x, y, 1)
+    y_equation = list(sorted([x_val * m + b for x_val in x]))
+    print("y = {:4}x + {:4}".format(m, b))
     plt.plot(list(sorted(x)), y_equation)
     plt.show()
