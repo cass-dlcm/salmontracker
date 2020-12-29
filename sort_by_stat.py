633beca32d5bc555ead396af62d6d7f2d991ae15
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2020-12-29 17:25:34 -0500

    make sort_by_stat use memory instead of disk

--- sort_by_stat.py
+++ sort_by_stat.py
@@ -6,12 +6,11 @@ from core import (
 )
 from objects import Job, Stage_WaterLevel_KnownOccurrence
 import filters
-import jsonlines
 import requests
 import pprint
 from typing import Tuple, List, Dict, Union, cast
 import ujson
-import gzip
+import zlib
 import os
 
 
@@ -54,24 +53,23 @@ def sortWeapons(data, stat) -> None:
     for weapon in weaponsList:
         print(weapon["key"])
         result: Dict[str, Union[str, float]] = {}
-        filterPaths: Tuple[str, str] = filters.hasWeapons(
-            data, cast(List[str], [weapon["main_ref"]])
+        filterPaths: Tuple[List[bytes], List[bytes]] = cast(
+            Tuple[List[bytes], List[bytes]],
+            filters.hasWeapons("mem", data, cast(List[str], [weapon["main_ref"]])),
         )
-        withVal: str = filterPaths[0]
-        withoutVal: str = filterPaths[1]
-        if hasJobs(withVal) and not hasVal(
+        withVal: List[bytes] = filterPaths[0]
+        withoutVal: List[bytes] = filterPaths[1]
+        if hasJobs("mem", withVal) and not hasVal(
             cast(List[Dict[str, str]], results), cast(str, weapon["main_ref"])
         ):
-            if (hasJobs(withVal)) and (hasJobs(withoutVal)):
+            if (hasJobs("mem", withVal)) and (hasJobs("mem", withoutVal)):
                 result["key"] = cast(str, weapon["main_ref"])
                 result["name"] = cast(Dict[str, str], weapon["name"])[locale]
                 result["value"] = (
-                    statSummary(withVal, stat)[0] - statSummary(withoutVal, stat)[0]
+                    statSummary("mem", withVal, stat)[0]
+                    - statSummary("mem", withoutVal, stat)[0]
                 )
                 results.append(result)
-        elif not hasJobs(withVal):
-            os.remove(withVal)
-            os.remove(withoutVal)
     pprint.pprint(sorted(results, key=lambda val: val["value"]))
 
 
@@ -86,55 +84,52 @@ def sortStages(data, stat) -> None:
     """
     stageDict: Dict[str, Dict[str, Union[str, float]]] = {}
     stageList: List[Dict[str, Union[str, float]]] = []
-    with gzip.open(data) as reader:
-        for line in reader:
-            job = Job(**ujson.loads(line))
-            if job.has_stage():
-                if not (
+    for line in data:
+        job = Job(**ujson.loads(zlib.decompress(line)))
+        if job.has_stage():
+            if not (
+                getattr(cast(Stage_WaterLevel_KnownOccurrence, job.stage).name, locale)
+                in stageDict
+            ):
+                stageDict[
                     getattr(
-                        cast(Stage_WaterLevel_KnownOccurrence, job.stage).name, locale
+                        cast(Stage_WaterLevel_KnownOccurrence, job.stage).name,
+                        locale,
                     )
-                    in stageDict
-                ):
-                    stageDict[
-                        getattr(
-                            cast(Stage_WaterLevel_KnownOccurrence, job.stage).name,
-                            locale,
-                        )
-                    ] = {
-                        "name": getattr(
-                            cast(Stage_WaterLevel_KnownOccurrence, job.stage).name,
-                            locale,
-                        ),
-                        stat: 0.0,
-                        "count": 0.0,
-                    }
-                cast(
-                    Dict[str, float],
-                    stageDict[
-                        getattr(
-                            cast(Stage_WaterLevel_KnownOccurrence, job.stage).name,
-                            locale,
-                        )
-                    ],
-                )[stat] += cast(float, getattr(job, stat))
-                cast(
-                    Dict[str, float],
-                    stageDict[
-                        getattr(
-                            cast(Stage_WaterLevel_KnownOccurrence, job.stage).name,
-                            locale,
-                        )
-                    ],
-                )["count"] += 1.0
-        for stage in stageDict.values():
-            stageList.append(
-                {
-                    "name": stage["name"],
-                    "value": cast(float, stage[stat]) / cast(float, stage["count"]),
+                ] = {
+                    "name": getattr(
+                        cast(Stage_WaterLevel_KnownOccurrence, job.stage).name,
+                        locale,
+                    ),
+                    stat: 0.0,
+                    "count": 0.0,
                 }
-            )
-        pprint.pprint(sorted(stageList, key=lambda val: val["value"]))
+            cast(
+                Dict[str, float],
+                stageDict[
+                    getattr(
+                        cast(Stage_WaterLevel_KnownOccurrence, job.stage).name,
+                        locale,
+                    )
+                ],
+            )[stat] += cast(float, core.getValMultiDimensional(job, [stat]))
+            cast(
+                Dict[str, float],
+                stageDict[
+                    getattr(
+                        cast(Stage_WaterLevel_KnownOccurrence, job.stage).name,
+                        locale,
+                    )
+                ],
+            )["count"] += 1.0
+    for stage in stageDict.values():
+        stageList.append(
+            {
+                "name": stage["name"],
+                "value": cast(float, stage[stat]) / cast(float, stage["count"]),
+            }
+        )
+    pprint.pprint(sorted(stageList, key=lambda val: val["value"]))
 
 
 def sortSpecial(data, stat) -> None:
@@ -148,31 +143,29 @@ def sortSpecial(data, stat) -> None:
     """
     specialDict: Dict[str, Dict[str, Union[str, float]]] = {}
     specialList: List[Dict[str, Union[str, float]]] = []
-    with gzip.open(data) as reader:
-        for line in reader:
-            job = Job(**ujson.loads(line))
-            if not (getattr(job.my_data.special.name, locale) in specialDict):
-                specialDict[getattr(job.my_data.special.name, locale)] = {
-                    "name": getattr(job.my_data.special.name, locale),
-                    stat: 0.0,
-                    "count": 0.0,
-                }
-            cast(
-                Dict[str, float],
-                specialDict[getattr(job.my_data.special.name, locale)],
-            )[stat] += cast(float, getattr(job, stat))
-            cast(
-                Dict[str, float],
-                specialDict[getattr(job.my_data.special.name, locale)],
-            )["count"] += 1.0
-        for special in specialDict.values():
-            specialList.append(
-                {
-                    "name": special["name"],
-                    "value": cast(float, special[stat]) / cast(float, special["count"]),
-                }
-            )
-        pprint.pprint(sorted(specialList, key=lambda val: val["value"]))
+    for line in data:
+        job = Job(**ujson.loads(zlib.decompress(line)))
+        if not (getattr(job.my_data.special.name, locale) in specialDict):
+            specialDict[getattr(job.my_data.special.name, locale)] = {
+                "name": getattr(job.my_data.special.name, locale),
+                stat: 0.0,
+                "count": 0.0,
+            }
+        cast(Dict[str, float], specialDict[getattr(job.my_data.special.name, locale)],)[
+            stat
+        ] += cast(float, core.getValMultiDimensional(job, [stat]))
+        cast(
+            Dict[str, float],
+            specialDict[getattr(job.my_data.special.name, locale)],
+        )["count"] += 1.0
+    for special in specialDict.values():
+        specialList.append(
+            {
+                "name": special["name"],
+                "value": cast(float, special[stat]) / cast(float, special["count"]),
+            }
+        )
+    pprint.pprint(sorted(specialList, key=lambda val: val["value"]))
 
 
 def sortRotation(data, stat) -> None:
@@ -189,29 +182,32 @@ def sortRotation(data, stat) -> None:
     rotationResultsList: List[
         Dict[str, Union[int, float, Union[None, Dict[str, Union[str, List[str]]]]]]
     ] = []
-    with gzip.open(data) as reader:
-        for job in jsonlines.Reader(reader, ujson.loads):
-            if job["shift_start_at"]["time"] not in rotationList:
-                rotationList.append(job["shift_start_at"]["time"])
+    for line in data:
+        job = Job(**ujson.loads(zlib.decompress(line)))
+        if job.shift_start_at.time not in rotationList:
+            rotationList.append(job.shift_start_at.time)
     for rotation in rotationList:
         print(rotation)
         result: Dict[
             str, Union[int, float, Union[None, Dict[str, Union[str, List[str]]]]]
         ] = {}
-        filterPaths: Tuple[str, str] = filters.duringRotationInts(data, [rotation])
-        withVal: str = filterPaths[0]
-        withoutVal: str = filterPaths[1]
-        if hasJobs(withVal):
-            if (hasJobs(withVal)) and (hasJobs(withoutVal)):
+        filterPaths: Tuple[List[bytes], List[bytes]] = cast(
+            Tuple[List[bytes], List[bytes]],
+            filters.duringRotationInts("mem", data, [rotation]),
+        )
+        withVal: List[bytes] = filterPaths[0]
+        withoutVal: List[bytes] = filterPaths[1]
+        if hasJobs("mem", withVal):
+            if (hasJobs("mem", withVal)) and (hasJobs("mem", withoutVal)):
                 result["name"] = rotation
-                result["data"] = core.findWeaponsAndStageByRotation(withVal, rotation)
+                result["data"] = core.findWeaponsAndStageByRotation(
+                    "mem", withVal, rotation
+                )
                 result["value"] = (
-                    statSummary(withVal, stat)[0] - statSummary(withoutVal, stat)[0]
+                    statSummary("mem", withVal, stat)[0]
+                    - statSummary("mem", withoutVal, stat)[0]
                 )
                 rotationResultsList.append(result)
-        elif not hasJobs(withVal):
-            os.remove(withVal)
-            os.remove(withoutVal)
     pprint.pprint(
         sorted(rotationResultsList, key=lambda val: cast(float, val["value"]))
     )
@@ -220,7 +216,7 @@ def sortRotation(data, stat) -> None:
 if __name__ == "__main__":
     # fullPath: str = core.init("User", "disk", ujson.load(open("keys.json", "r"))["statink_key"])
     fullPath: str = cast(str, core.init("All", "disk"))
-    # sortStages(fullPath, "clear_waves")
-    sortWeapons(fullPath, "clear_waves")
-    # sortSpecial(fullPath, "clear_waves")
-    # sortRotation(fullPath, "clear_waves")
+    # sortStages(core.loadJobsFromFile(fullPath), "clear_waves")
+    sortWeapons(core.loadJobsFromFile(fullPath), "clear_waves")
+    # sortSpecial(core.loadJobsFromFile(fullPath), "clear_waves")
+    # sortRotation(core.loadJobsFromFile(fullPath), "clear_waves")
